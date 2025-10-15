81bb32b7ae410a1cfc9c0b410352d5c28c164043
Uendson Santos <uendsonsantos09@gmail.com>
2025-10-15 17:02:59 +0000

    Criação das variáveis e registros das cartas 1 e 2.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,15 +1,95 @@
 #include <stdio.h>
+int main () {
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das cartas
-// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
+    // Declaração de variaveis das cartas 1 //
 
-int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+    char Estado1[100];
+    char Codigodacarta1[100];
+    char Nomedacidade1[150];
+    int População1;
+    float Area1;
+    float PIB1;
+    int PontosTuristicos1;
 
-  // Área para entrada de dados
+// Entrada de dados das Cartas 1 //
 
-  // Área para exibição dos dados da cidade
+printf("Digite o Estado da carta 1: \n");
+scanf(" %s", Estado1);
+
+printf("Digite o Código da Carta 1: \n");
+scanf(" %s", Codigodacarta1);
+
+printf("Digite o Nome da Cidade da carta 1: \n");
+scanf(" %[^\n]", Nomedacidade1);
+
+printf("Digite a População da carta 1: \n");
+scanf("%d", &População1);
+
+printf("Digite a Área da carta 1: \n");
+scanf("%f", &Area1);
+
+printf("Digite o PIB da carta 1: \n");
+scanf("%f", &PIB1);
+
+printf("Digite os Pontos Turisticos da carta 1: \n");
+scanf("%d", &PontosTuristicos1);
+
+// Exibição das Cartas 1 //
+
+printf("\n===== DADOS DA CARTA 1 =====\n");
+printf("Estado: %s\n", Estado1);
+printf("Código da carta: %s\n", Codigodacarta1);
+printf("Cidade: %s\n", Nomedacidade1);
+printf("População: %d\n", População1);
+printf("Área: %.2f km²\n", Area1);
+printf("PIB: %.2f bilhões\n", PIB1);
+printf("Pontos turísticos: %d\n", PontosTuristicos1);
+printf("=============================\n");
+
+// Declaração das variaveis da carta 2 //
+
+char Estado2[200];
+char Codigodacarta2[200];
+char Nomedacidade2[300];
+int População2;
+float Area2;
+float PIB2;
+int PontosTuristicos2;
+
+// Entrada de dados das Cartas 2 //
+
+printf("Digite o Estado da carta 2: \n");
+scanf(" %s", Estado2);
+
+printf("Digite o Código da Carta 2: \n");
+scanf(" %s", Codigodacarta2);
+
+printf("Digite o Nome da Cidade da carta 2: \n");
+scanf(" %[^\n]", Nomedacidade2);
+
+printf("Digite a População da carta 2: \n");
+scanf("%d", &População2);
+
+printf("Digite a Área da carta 2: \n");
+scanf("%f", &Area2);
+
+printf("Digite o PIB da carta 2: \n");
+scanf("%f", &PIB2);
+
+printf("Digite os Pontos Turisticos da carta 2: \n");
+scanf("%d", &PontosTuristicos2);
+
+// Exibição das Cartas 2 //
+
+printf("\n===== DADOS DA CARTA 2 =====\n");
+printf("Estado: %s\n", Estado2);
+printf("Código da carta: %s\n", Codigodacarta2);
+printf("Cidade: %s\n", Nomedacidade2);
+printf("População: %d\n", População2);
+printf("Área: %.2f km²\n", Area2);
+printf("PIB: %.2f bilhões\n", PIB2);
+printf("Pontos turísticos: %d\n", PontosTuristicos2);
+printf("=============================\n");
 
 return 0;
-} 
+}
